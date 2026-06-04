38e383de91df0cb12049d2d4d3381d45165793d4
SrishtiGupta3 <gsrishti252@gmail.com>
2026-06-04 21:57:57 +0530

    Add files via upload

---
+++ Day3/LCM.c
@@ -0,0 +1,15 @@
+// Ques 4 lcm of two numbers
+#include<stdio.h>
+int main(){
+    int num2,num1,i,y=1,lcm;
+    printf("Enter Value of numbers:");
+    scanf("%d\n%d",&num1,&num2);
+    for(i=1;i<=num1&&i<=num2;i++){
+        if((num1%i==0)&&(num2%i==0)){
+           y=i*y;
+        }
+        }
+         lcm=((num1)*(num2))/y;
+        printf("LCM is :%d",lcm);  
+return 0;
+}
