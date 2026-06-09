6760d78090cc8385c0908aa12176bb69cfed7e0f
SrishtiGupta3 <gsrishti252@gmail.com>
2026-06-09 18:44:00 +0530

    Add files via upload

---
+++ Day9/Ques1.c
@@ -0,0 +1,17 @@
+// Reverse Star PAttern
+#include<stdio.h>
+int main(){
+   int i,j;
+     for(i=1;i<=5;i++){
+         for(j=1;j<=5;j++){
+             if(j<=(6-i)){
+                 printf("*\t");
+             }
+             else{
+                 printf("\t");
+             }
+         }
+         printf("\n");
+     }
+     return 0;
+    }
